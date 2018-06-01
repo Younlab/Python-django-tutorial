55ec358863d74240b72b7e34ae2ce3c78298b76f
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-01 16:55:17 +0900

    미완성

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from os import path
 
+from django.http import HttpResponse
+import os
 # Create your views here.
 def post_list(request):
-    return HttpResponse('Post List 입니다.')
+
+    return HttpResponse()
+
+
+
+
+
