a8bfe82dfb6c3dbc1842e0af5bbcca8b8bbb5e36
Sehyeon Youn <d.sehyeon@gmail.com>
2018-06-04 15:08:24 +0900

    post_list에서 동적으로 글목록 문자열을 만들어 리턴해보기

--- app/blog/views.py
+++ app/blog/views.py
@@ -3,7 +3,17 @@ from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Post
+
 
 def post_list(request):
-    html = render_to_string('blog/post_list.html')
-    return render(request, 'blog/post_list.html')
+    # html = render_to_string('blog/post_list.html')
+    # posts = Post.objects.all()
+    #
+    # result = 'index<br>'
+    #
+    # for post in posts:
+    #     result += f'-{post}<br>'
+    #
+    # # return HttpResponse(result)
+    # return HttpResponse(result)
